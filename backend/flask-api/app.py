a2f08d9086efa700bbeba54a3465982027567dd3
joaquimYuste <yuste321@gmail.com>
2020-10-20 19:28:38 +0200

    Account model added

--- backend/flask-api/app.py
+++ backend/flask-api/app.py
@@ -1,7 +1,21 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+#models
+from models.accounts import AccountModel
+
+from db import db
+
 app = Flask(__name__)
 
+#Database config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
 
 @app.route('/')
 def hello_world():
