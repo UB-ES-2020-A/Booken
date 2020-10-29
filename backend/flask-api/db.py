9bf56378c9f95760119f9c72695c42a6ba01862a
Rodrigo Cabezas Quirós <rcmikado@gmail.com>
2020-10-29 20:23:05 +0100

    test

--- backend/flask-api/db.py
+++ backend/flask-api/db.py
@@ -8,7 +8,9 @@ db = SQLAlchemy()
 secret_key = "McQfTjWnZr4u7x!A%D*G-KaPdRgUkXp2s5v8y/B?E(H+MbQeThVmYq3t6w9z$C&F"
 
 def create_app():
-    app = Flask(__name__)
+    app = app = Flask(__name__,
+                static_folder="../front-end/dist/static",
+                template_folder="../front-end/dist")
     app.config.from_object(__name__)
     # Cross-origin request config
     CORS(app, resources={r'/*': {'origins': '*'}})
