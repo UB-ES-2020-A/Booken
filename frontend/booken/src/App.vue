50276beac3d96ba8f1c409a1957d71a6200d438c
Rodrigo Cabezas Quirós <rcmikado@gmail.com>
2020-11-05 20:16:27 +0100

    fix qj

--- frontend/booken/src/App.vue
+++ frontend/booken/src/App.vue
@@ -145,38 +145,32 @@
           <div class="nav navbar-nav mx-auto">
             <router-link :to="{name: 'books', params: {category: 'HUMANIDADES'}}"
                          class="nav-item nav-link categoriestxt" active-class="active"
-                         exact @click="toggledNav = !toggledNav">Humanidades
+                         >Humanidades
             </router-link>
             <router-link :to="{name: 'books', params: {category: 'TECNICO Y FORMACION'}}"
                          class="nav-item nav-link categoriestxt" active-class="active"
-                         exact @click="toggledNav = !toggledNav">Técnico y formación
+                         >Técnico y formación
             </router-link>
             <router-link :to="{name: 'books', params: {category: 'METODOS DE IDIOMAS'}}"
                          class="nav-item nav-link categoriestxt" active-class="active"
-                         exact @click="toggledNav = !toggledNav">Métodos de idiomas
+                         >Métodos de idiomas
             </router-link>
             <router-link class="nav-item nav-link categoriestxt" active-class="active"
-                         exact @click="toggledNav = !toggledNav"
                          :to="{name: 'books', params: {category: 'LITERATURA'}}">Literatura
             </router-link>
             <router-link class="nav-item nav-link categoriestxt" active-class="active"
-                         exact @click="toggledNav = !toggledNav"
                          :to="{name: 'books', params: {category: 'INFANTIL'}}">Infantil
             </router-link>
             <router-link class="nav-item nav-link categoriestxt" active-class="active"
-                         exact @click="toggledNav = !toggledNav"
                          :to="{name: 'books', params: {category: 'COMICS Y MANGA'}}">Cómics y manga
             </router-link>
             <router-link class="nav-item nav-link categoriestxt" active-class="active"
-                         exact @click="toggledNav = !toggledNav"
                          :to="{name: 'books', params: {category: 'JUVENIL'}}">Juvenil
             </router-link>
             <router-link class="nav-item nav-link categoriestxt" active-class="active"
-                         exact @click="toggledNav = !toggledNav"
                          :to="{name: 'books', params: {category: 'OTRAS CATEGORIAS'}}">Otras categorías
             </router-link>
             <router-link class="nav-link categoriestxt" active-class="active"
-                         exact @click="toggledNav = !toggledNav"
                          :to="{name: 'books', params: {category: 'TODO'}}"><b style="color: yellow">Ver todo</b>
             </router-link>
           </div>
