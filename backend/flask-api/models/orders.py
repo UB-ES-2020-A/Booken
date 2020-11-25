0e9d40bbfcc4440807db308aa2b281d863706395
david <davidfernandezfernandez.1999@gmail.com>
2020-11-25 20:24:27 +0100

    Send type and card id added to order model and post resource

--- backend/flask-api/models/orders.py
+++ backend/flask-api/models/orders.py
@@ -2,38 +2,42 @@ from db import db
 from models.book import BookModel
 
 from models.address import AddressModel
-
+from models.payment_card import CardModel
 articles = db.Table('relationship', db.Column('article_id', db.Integer, db.ForeignKey('articles.id')),
-                   db.Column('order_id', db.Integer, db.ForeignKey('orders.id')))
+                    db.Column('order_id', db.Integer, db.ForeignKey('orders.id')))
 
 address = db.Table('relationship2', db.Column('addresses_id', db.Integer, db.ForeignKey('addresses.id')),
                    db.Column('order_id', db.Integer, db.ForeignKey('orders.id')))
+
 
 class OrdersModel(db.Model):
     __tablename__ = 'orders'
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
-    id_user = db.Column(db.String(30), db.ForeignKey('accounts.id'),primary_key=False, nullable=False)
+    id_user = db.Column(db.String(30), db.ForeignKey('accounts.id'), primary_key=False, nullable=False)
     date = db.Column(db.String(30), primary_key=False, unique=False, nullable=False)
-    total = db.Column(db.Float, primary_key=False,nullable=False)
-    shipping = db.Column(db.Float, primary_key=False,nullable=False)
-    taxes = db.Column(db.Float,primary_key=False, nullable=False)
-    state = db.Column(db.Integer, primary_key=False,nullable=False)#0:in progress,1:sending,2:Received
-    #Address de la order
+    total = db.Column(db.Float, primary_key=False, nullable=False)
+    shipping = db.Column(db.Float, primary_key=False, nullable=False)
+    taxes = db.Column(db.Float, primary_key=False, nullable=False)
+    state = db.Column(db.Integer, primary_key=False, nullable=False)  # 0:in progress,1:sending,2:Received
+    send_type = db.Column(db.Integer, primary_key=False, nullable=False)  # 0:estandar,1:estandar plus,2:express
+    # Address de la order
     address = db.relationship('AddressModel', secondary=address, backref=db.backref('orders', lazy='dynamic'))
-    #Articles de la order
+    # Articles de la order
     articles = db.relationship('ArticlesModel', secondary=articles, backref=db.backref('orders', lazy='dynamic'))
 
-    #Card de pagament
-    #card = db.relationship('CardModel', secondary="card", backref='orders', lazy=True)
+    # Card de pagament
+    card_id = db.Column(db.Integer, primary_key=False, nullable=False)
 
-    def __init__(self, id_user ,date, total,shipping, taxes, state):
+    def __init__(self, id_user, date, total, shipping, taxes, state, send_type,card_id):
         self.id_user = id_user
         self.date = date
         self.total = total
         self.shipping = shipping
         self.taxes = taxes
         self.state = state
+        self.send_type = send_type
+        self.card_id  = card_id
 
     def json(self):
         articles_json = [article.json() for article in self.articles]
@@ -46,13 +50,16 @@ class OrdersModel(db.Model):
             "shipping": self.shipping,
             "taxes": self.taxes,
             "state": self.state,
+            "send_type": self.send_type,
+            "card_id":self.card_id,
             "address": address_json,
-            "articles": articles_json
+            "articles": articles_json,
         }
 
     def json_with_address_id(self):
         articles_json = [article.json() for article in self.articles]
         address_json = [address.json_with_id() for address in self.address]
+        card_json = [item.json() for item in self.card]
         return {
             "id": self.id,
             "id_user": self.id_user,
@@ -61,8 +68,11 @@ class OrdersModel(db.Model):
             "shipping": self.shipping,
             "taxes": self.taxes,
             "state": self.state,
+            "send_type": self.send_type,
+            "card_id": self.card_id,
             "address": address_json,
-            "articles": articles_json
+            "articles": articles_json,
+            "card": card_json
         }
 
     def json_filtered_by_book_id(self):
@@ -72,7 +82,9 @@ class OrdersModel(db.Model):
             "id_user": self.id_user,
             "book_name": book.name,
             "num_books": self.num_books,
-            "state": self.state
+            "state": self.state,
+            "send_type": self.send_type,
+            "card_id": self.card_id
         }
 
     def save_to_db(self):
@@ -87,16 +99,16 @@ class OrdersModel(db.Model):
         self.state = new_state
 
     @classmethod
-    def find_by_state(cls, state,id):
+    def find_by_state(cls, state, id):
         try:
             return OrdersModel.query.filter_by(state=state, id_user=id).all()
         except:
             return None
 
     @classmethod
-    def find_by_id_user_and_orderid(cls, id_user,order_id):
+    def find_by_id_user_and_orderid(cls, id_user, order_id):
         try:
-            return OrdersModel.query.filter_by(id_user=id_user, id = order_id).all()
+            return OrdersModel.query.filter_by(id_user=id_user, id=order_id).all()
         except:
             return None
 
@@ -123,7 +135,7 @@ class OrdersModel(db.Model):
         list_orders = [order.json() for order in OrdersModel.query.all()]
         dicc = {"orders": list_orders}
         return dicc
-      
+
     def add_article(self, article):
         self.articles += [article]
         self.save_to_db()
@@ -151,6 +163,10 @@ class OrdersModel(db.Model):
             return 1
         else:
             return 0
+
+    def get_card(self):
+        card = CardModel.find_by_id(self.card_id)
+        return card.json()
+
     def get_num_articles(self):
         return len(self.articles)
-
