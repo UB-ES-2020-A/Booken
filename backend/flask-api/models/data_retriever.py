dfe722f86b610400fe027c853e78174ef1bde846
joaquimYuste <yuste321@gmail.com>
2020-11-29 13:03:31 +0100

    Cleaned code

--- backend/flask-api/models/data_retriever.py
+++ backend/flask-api/models/data_retriever.py
@@ -3,31 +3,31 @@ from models.orders import OrdersModel
 class DataRetriever():
 
     @classmethod
-    def get_data(self,needed_data):
+    def get_data(cls,needed_data):
         if needed_data == "total_sales":
-            return self.total_sales(self)
+            return cls._total_sales(cls)
         elif needed_data == "sales_month":
-            return self.sales_month(self)
+            return cls._sales_month(cls)
         elif needed_data == "sales_year":
-            return self.sales_year(self)
+            return cls._sales_year(cls)
         elif needed_data == "sales_genre":
-            return self.sales_genre(self)
+            return cls._sales_genre(cls)
         elif needed_data == "total_gain":
-            return self.total_gain(self)
+            return cls._total_gain(cls)
         elif needed_data == "gain_month":
-            return self.gain_month(self)
+            return cls._gain_month(cls)
         elif needed_data == "gain_year":
-            return self.gain_year(self)
+            return cls._gain_year(cls)
         elif needed_data == "gain_genre":
-            return self.gain_genre(self)
+            return cls._gain_genre(cls)
         else:
-            return
+            return None
 
-    def get_orders(self):
+    def _get_orders(self):
         return OrdersModel.get_orders()['orders']
 
-    def total_sales(self):
-        orders = self.get_orders(self)
+    def _total_sales(self):
+        orders = self._get_orders(self)
         n_sales = 0
         for order in orders:
             for article in order['articles']:
@@ -35,8 +35,8 @@ class DataRetriever():
 
         return {'sales': n_sales}
 
-    def sales_month(self):
-        orders = self.get_orders(self)
+    def _sales_month(self):
+        orders = self._get_orders(self)
         n_sales_month = {}
 
         for order in orders:
@@ -44,13 +44,13 @@ class DataRetriever():
             for article in order['articles']:
                 try:
                     n_sales_month[month] += int(article['quant'])
-                except:
+                except KeyError:
                     n_sales_month[month] = int(article['quant'])
 
         return n_sales_month
 
-    def sales_year(self):
-        orders = self.get_orders(self)
+    def _sales_year(self):
+        orders = self._get_orders(self)
         n_sales_year = {}
 
         for order in orders:
@@ -58,13 +58,13 @@ class DataRetriever():
             for article in order['articles']:
                 try:
                     n_sales_year[year] += int(article['quant'])
-                except:
+                except KeyError:
                     n_sales_year[year] = int(article['quant'])
 
         return n_sales_year
 
-    def sales_genre(self):
-        orders = self.get_orders(self)
+    def _sales_genre(self):
+        orders = self._get_orders(self)
         n_sales_genre = {}
 
         for order in orders:
@@ -72,13 +72,13 @@ class DataRetriever():
                 genre = article['categoria']
                 try:
                     n_sales_genre[genre] += article['quant']
-                except:
+                except KeyError:
                     n_sales_genre[genre] = article['quant']
 
         return n_sales_genre
 
-    def total_gain(self):
-        orders = self.get_orders(self)
+    def _total_gain(self):
+        orders = self._get_orders(self)
         total_gain = 0
 
         for order in orders:
@@ -86,8 +86,8 @@ class DataRetriever():
 
         return {'gain': total_gain}
 
-    def gain_month(self):
-        orders = self.get_orders(self)
+    def _gain_month(self):
+        orders = self._get_orders(self)
 
         gain_month = {}
 
@@ -95,13 +95,13 @@ class DataRetriever():
             month = order['date'][-7:]
             try:
                 gain_month[month] += order['total']
-            except:
+            except KeyError:
                 gain_month[month] = order['total']
 
         return gain_month
 
-    def gain_year(self):
-        orders = self.get_orders(self)
+    def _gain_year(self):
+        orders = self._get_orders(self)
 
         gain_month = {}
 
@@ -109,13 +109,13 @@ class DataRetriever():
             year = order['date'][-4:]
             try:
                 gain_month[year] += order['total']
-            except:
+            except KeyError:
                 gain_month[year] = order['total']
 
         return gain_month
 
-    def gain_genre(self):
-        orders = self.get_orders(self)
+    def _gain_genre(self):
+        orders = self._get_orders(self)
 
         gain_genre = {}
 
@@ -124,7 +124,7 @@ class DataRetriever():
                 genre = article['categoria']
                 try:
                     gain_genre[genre] += float(article['price'])
-                except:
+                except KeyError:
                     gain_genre[genre] = float(article['price'])
 
         return gain_genre
