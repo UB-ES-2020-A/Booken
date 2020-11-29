2fde7d23fda8a7566aaf2e5f2b4a2336299f9b33
joaquimYuste <yuste321@gmail.com>
2020-11-29 22:50:36 +0100

    Coverage's tests added

---
+++ backend/flask-api/tests/Account_test.py
@@ -0,0 +1,47 @@
+import unittest
+import sys
+import json
+sys.path.append('../')
+from app import setupApp
+from db import db
+
+
+class AccountTests(unittest.TestCase):
+
+    def setUp(self):
+
+        self.app = setupApp().test_client()
+        db.drop_all()
+        db.create_all()
+
+    def tearDown(self):
+        # Executed after each test
+        pass
+
+    def test_non_registered_login(self):
+        response = self.login('lelaco@ub.edu', '12354asuidjh')
+        self.assertEqual(response.status_code, 404)
+
+    def test_register_account(self):
+        response = self.register('Cristobal', 'Colon', 'tengo@barcos.tech', 'america16')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_user_login(self):
+        self.register('Cristobal', 'Colon', 'tengo@barcos.tech', 'america16')
+        response = self.login('tengo@barcos.tech', 'america16')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(json(response.data)['token'], '')
+
+    def register(self, name, lname, email, password):
+        return self.app.post('/account',
+                             data=dict(name=name, lastname=lname, email=email, password=password),
+                             follow_redirects=True)
+
+    def login(self, email, password):
+        return self.app.post('/login',
+                             data=dict(email=email, password=password),
+                             follow_redirects=True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ backend/flask-api/tests/Address_test.py
@@ -0,0 +1,92 @@
+import unittest
+import sys
+import json
+
+sys.path.append('../')
+from app import setupApp
+from db import db
+
+
+class AddressTests(unittest.TestCase):
+
+    address_info = {
+        "id":1,
+        "label_name":"Mi casa",
+        "name":"test",
+        "surnames":"tests",
+        "street":"La calle de mi casa",
+        "number":32,
+        "cp":"08431",
+        "city":"Mi city",
+        "province":"Mi provincia",
+        "telf":123456
+    }
+
+    def setUp(self):
+
+        self.app = setupApp().test_client()
+        db.drop_all()
+        db.create_all()
+
+        self.app.post('/account',
+                      data=dict(name="test", lastname="test", email="test", password="test"),
+                      follow_redirects=True)
+
+    def tearDown(self):
+        # Executed after each test
+        pass
+
+    def test_create_address(self):
+        response = self.add_address(self.address_info)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_concrete_address(self):
+        self.add_address(self.address_info)
+        response = self.app.get('/account/1/address/1', follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.address_info, json.loads(response.data)["address"])
+
+    def test_get_addresses(self):
+        self.add_address(self.address_info)
+        self.add_address(self.address_info)
+        response = self.app.get('/account/1/addresses', follow_redirects=True)
+
+        tmp_address_1 = self.address_info.copy()
+        tmp_address_2 = self.address_info.copy()
+
+        tmp_address_1["id"] = 1
+        tmp_address_2["id"] = 2
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data)["accounts_addresses"], [tmp_address_1,tmp_address_2])
+
+    def test_modify_address(self):
+        self.add_address(self.address_info)
+
+        tmp_address = self.address_info.copy()
+        tmp_address["label_name"] = "La casa del vecino"
+
+        self.app.put('/account/1/address/1', data=tmp_address, follow_redirects=True)
+
+        response = self.app.get('/account/1/address/1', follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(tmp_address, json.loads(response.data)["address"])
+
+
+    def test_delete_address(self):
+        self.add_address(self.address_info)
+
+        response = self.app.delete('/account/1/address/1', follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
+
+
+    def add_address(self, info):
+        return self.app.post('/account/1/address',
+                             data=info,
+                             follow_redirects=True)
+
+if __name__ == '__main__':
+    unittest.main()
