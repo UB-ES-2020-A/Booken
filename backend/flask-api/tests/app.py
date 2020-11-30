26fa83bb165b5a89d3d87952b43ffeae4cee1125
Rodrigo Cabezas Quirós <rcmikado@gmail.com>
2020-11-30 01:08:17 +0100

    mcr-comm.

--- backend/flask-api/tests/app.py
+++ backend/flask-api/tests/app.py
@@ -3,7 +3,7 @@ import sys
 import json
 import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '../')
+sys.path.insert(1, '../')
 from flask_migrate import Migrate
 from flask_restful import Api
 from resources.account_rs import Account, Accounts, PasswordChange
