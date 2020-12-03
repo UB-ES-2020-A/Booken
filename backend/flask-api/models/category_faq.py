2964184bd8c3bb1a41a2cf9b68dfb820b4181496
david <davidfernandezfernandez.1999@gmail.com>
2020-12-03 15:21:33 +0100

    Fin models and resources

--- backend/flask-api/models/category_faq.py
+++ backend/flask-api/models/category_faq.py
@@ -6,15 +6,15 @@ class CategoryModel(db.Model):
     __tablename__ = 'category'
     id = db.Column(db.Integer, unique=True, primary_key=True)
     type = db.Column(db.String(30), unique=False, nullable=False)
-
+    num_faq = db.Column(db.Integer)
     def __init__(self, type_category):
         self.type = type_category
-
-
+        self.num_faq = 0
     def json(self):
         return {
             "id": self.id,
             "type": self.type,
+            "num_faq": self.num_faq
         }
 
     def save_to_db(self):
@@ -25,6 +25,8 @@ class CategoryModel(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+    def add_faq(self):
+        self.num_faq+=1
     @classmethod
     def find_by_id(cls, idd):
         return CategoryModel.query.filter_by(id=idd).first()
