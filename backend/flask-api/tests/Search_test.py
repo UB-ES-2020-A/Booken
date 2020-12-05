7ad711dac871d98b56f8f8a111585c486e950d7c
Abdelkarim Azzouguagh <abdel.azzouguagh@gmail.com>
2020-12-05 19:22:20 +0100

    cov search test 100

--- backend/flask-api/tests/Search_test.py
+++ backend/flask-api/tests/Search_test.py
@@ -37,27 +37,6 @@ class SearchBookTests(unittest.TestCase):
         "cover_image_url": 'asd',
         "back_cover_image_url": 'asd'
     }
-    book_info2 = {
-        "isbn": 12345679736,
-        "name": 'The secret of the A',
-        "author_name": 'Kim Follet',
-        "author_bd": '10/10/1979',
-        "author_city": "BCN",
-        "author_country": "Spain",
-        "genre": 'HUMANIDADES',
-        "year": 2020,
-        "editorial": 'Santillana',
-        "language": 'Castellano',
-        "price": 10.9,
-        "synopsis": 'Synopsis',
-        "description": 'Descripcion',
-        "num_pages": 130,
-        "cover_type": 0,
-        "num_sales": 0,
-        "total_available": 15,
-        "cover_image_url": 'asd',
-        "back_cover_image_url": 'asd'
-    }
 
     def setUp(self):
         self.app = setupApp(test=True).test_client()
@@ -70,40 +49,28 @@ class SearchBookTests(unittest.TestCase):
 
     def test_search_by_name(self):
         self.postBook(self.book_info1)
-        self.postBook(self.book_info2)
         lista = ['the', 'sec', 'secret', 'of', 'stones']
         rand = random.choices(lista)
         response = self.search(rand)
         self.assertEqual(response.status_code, 200)
-        if rand == ['stones']:
-            self.assertIn(b'The secret of the Stones', response.data)
-        else:
-            self.assertIn(b'The secret of the Stones', response.data)
-            self.assertIn(b'The secret of the A', response.data)
+        self.assertIn(b'The secret of the Stones', response.data)
 
     def test_search_by_isbn(self):
         self.postBook(self.book_info1)
-        self.postBook(self.book_info2)
         lista = [12, 123, 567, 123456789]
         rand = random.choices(lista)
         response = self.search(rand)
         self.assertEqual(response.status_code, 200)
-        if rand == [123456789]:
-            self.assertIn(b'12345678911', response.data)
-            self.assertIn(b'The secret of the Stones', response.data)
-        else:
-            self.assertIn(b'12345679736', response.data)
-            self.assertIn(b'The secret of the A', response.data)
-            self.assertIn(b'The secret of the Stones', response.data)
+        self.assertIn(b'12345678911', response.data)
+        self.assertIn(b'The secret of the Stones', response.data)
+
 
     def test_search_by_author_name(self):
         self.postBook(self.book_info1)
-        self.postBook(self.book_info2)
         lista = ['kim', 'foll', 'follet']
         response = self.search(random.choices(lista))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Kim Follet', response.data)
-        self.assertIn(b'The secret of the A', response.data)
         self.assertIn(b'The secret of the Stones', response.data)
 
     def postBook(self, info):
