67619412f2ab7a38746440a5b7ae059f5e2edd8b
Rodrigo Cabezas Quirós <rcmikado@gmail.com>
2020-12-06 21:19:28 +0100

    mcr-comm.

--- backend/flask-api/tests/App_test.py
+++ backend/flask-api/tests/App_test.py
@@ -1,34 +1,37 @@
 import os
 import sys
+import threading
+import time
 import unittest
-import signal
+
 
 parent_path = os.path.dirname(os.path.abspath(__file__))[:-6]
 sys.path.insert(1, parent_path)
 from app import setupApp
 
 
-class TimeOutException(Exception):
-    def __init__(self, message, errors):
-        super(TimeOutException, self).__init__(message)
-        self.errors = errors
+class Alarm (threading.Thread):
+    def __init__ (self, timeout):
+        threading.Thread.__init__ (self)
+        self.timeout = timeout
+        self.setDaemon (True)
+    def run (self):
+        setupApp(True).run()
+        time.sleep(self.timeout)
+
 
 
 class MyTestCase(unittest.TestCase):
 
-    app = setupApp(True)
-
     def test_index_gets(self):
-        app = self.app.test_client()
+        app = setupApp(True).test_client()
         response = app.get('/', follow_redirects=True)
         self.assertEqual(200, response.status_code)
         response = app.get('/asd', follow_redirects=True)
         self.assertEqual(404, response.status_code)
 
     def test_app_runs(self):
-        try:
-            signal.alarm(2)
-            self.app.run()
-        except TimeOutException:
-            self.assertEqual("isCool", "isCool")
-
+        alarm = Alarm(4)
+        alarm.start()
+        del alarm
+        self.assertEqual(1, 1)
