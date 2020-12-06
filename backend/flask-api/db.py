4cce3800568d54afbfd1f0d9c48c4ab9593a83b1
joaquimYuste <yuste321@gmail.com>
2020-12-06 16:18:40 +0100

    Mail tests added

--- backend/flask-api/db.py
+++ backend/flask-api/db.py
@@ -23,8 +23,8 @@ def create_app(test=False):
         app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-        #app.config['MAIL_SUPPRESS_SEND'] = False
-        #email_templates["response_template"] = "../" + email_templates["response_template"]
+        app.config['MAIL_SUPPRESS_SEND'] = False
+        email_templates["response_template"] = "backend/flask-api/" + email_templates["response_template"]
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
