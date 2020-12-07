0e13fa0ba60abb1bb75e60a0a21c8f3a846705cc
Rodrigo Cabezas Quirós <rcmikado@gmail.com>
2020-12-08 00:20:00 +0100

    shopmanager UI front page customizable done

--- frontend/booken/src/components/Front.vue
+++ frontend/booken/src/components/Front.vue
@@ -20,6 +20,14 @@
           <!-- in case it is a jumbotron -->
           <div>
             <div role="group" style="text-align: right; margin-bottom: 0.1em" v-if="this.type == 2">
+              <button class="btn btn-success my-2 my-sm-0 mr-1" type="submit" v-if="item.id > 1" @click="moveUp(id)"
+                      style=""><i class="fas fa-arrow-up"
+                                  style="color: #FFF; font-size: 1.5em"/><a
+                  class="navbartextbt"></a></button>
+              <button class="btn btn-success my-2 my-sm-0 mr-1" type="submit" v-if="item.id < this.frontElements.length"
+                      style="" @click="moveDown(id)"><i class="fas fa-arrow-down"
+                                  style="color: #FFF; font-size: 1.5em"/><a
+                  class="navbartextbt"></a></button>
               <button class="btn btn-warning my-2 my-sm-0 mr-1" type="submit" @click="editSection(item.id)"
                       style=""><i class="fas fa-edit"
                                   style="color: #FFF; font-size: 1.5em; margin-right: 0.5em"/><a
@@ -37,11 +45,12 @@
                 <p class="lead" style="color: white">
                   {{ item.t1Sub }}
                 </p>
-                <hr class="my-4">
+                <hr class="my-4" v-if="item.t1Separator">
                 <p style="font-size: 0.6em; color: white">{{ item.t1Smalll }}</p>
               </div>
             </a>
           </div>
+
           <!-- in case it is a custom row -->
           <div v-if="item.frontType==2">
             <h2>{{ item.t2RowTitle }}</h2>
@@ -97,22 +106,23 @@
               </div>
               <div class="col" style="text-align: right">
                 <button class="btn btn-danger my-2 my-sm-0 mr-2" type="submit"
-                        v-if="edit"><i class="fas fa-times" style="color: #FFF; font-size: 1.5em; margin-right: 0.5em"/><a
-                    class="navbartextbt" @click="discardChanges">Descartar</a></button>
+                        v-if="edit" @click="discardChanges"><i class="fas fa-times"
+                                                               style="color: #FFF; font-size: 1.5em; margin-right: 0.5em"/><a
+                    class="navbartextbt">Descartar</a></button>
                 <button class="btn btn-warning my-2 my-sm-0 mr-2" type="submit"
-                        v-if="edit"><i class="fas fa-save"
-                                       style="color: #FFF; font-size: 1.5em; margin-right: 0.5em"/><a
-                    class="navbartextbt" @click="saveSection">Guardar</a></button>
+                        v-if="edit" @click="saveSection" :disabled="canSectionBeSaved()"><i class="fas fa-save"
+                                                                                            style="color: #FFF; font-size: 1.5em; margin-right: 0.5em"/><a
+                    class="navbartextbt">Guardar</a></button>
               </div>
             </div>
             <select class="form-control" v-model="this.addSectionValues.frontType">
-              <option value=-1>Seleccionar</option>
+              <option value=-1 disabled>Seleccionar</option>
               <option value=1>Banner</option>
               <option value=2>Fila de libros</option>
             </select>
             <div v-if="this.addSectionValues.frontType == 1" style="margin-top: 2em">
               <h3 class="card-subtitle">Configuración del banner</h3>
-              <h5 style="margin-top: 1em">Tipo de fondo del banner</h5>
+              <h5 style="margin-top: 1em">Tipo de fondo del banner*</h5>
               <div>
                 <div class="form-check form-check-inline">
                   <input class="form-check-input" type="radio" value=1 v-model="this.addBackgroundType">
@@ -124,31 +134,115 @@
                 </div>
               </div>
               <div v-if="this.addBackgroundType==1" style="margin-top: 0.5em">
-                <h5>URL imagen</h5>
+                <h5>URL imagen*</h5>
                 <input class="form-control" type="text" v-model="this.addSectionValues.t1BackgndURL">
               </div>
               <div v-if="this.addBackgroundType==2" style="margin-top: 0.5em">
-                <h5>Color</h5>
+                <h5>Color*</h5>
                 <input type="color" v-model="this.addSectionValues.t1BackgnCOL">
               </div>
-              <div style="margin-top: 0.5em">
+              <div style="margin-top: 0.5em" v-if="this.addBackgroundType != -1">
                 <h5>Título</h5>
                 <input class="form-control" type="text" v-model="this.addSectionValues.t1Tit">
               </div>
-              <div style="margin-top: 0.5em">
+              <div style="margin-top: 0.5em" v-if="this.addBackgroundType != -1">
                 <h5>Subtítulo</h5>
                 <input class="form-control" type="text" v-model="this.addSectionValues.t1Sub">
               </div>
-              <div style="margin-top: 0.5em">
-                <h5>Subtítulo</h5>
+              <div style="margin-top: 0.5em" v-if="this.addBackgroundType != -1">
+                <h5>Tercera línea</h5>
                 <input class="form-control" type="text" v-model="this.addSectionValues.t1Smalll">
               </div>
-              <div style="margin-top: 0.5em">
+              <div style="margin-top: 0.5em" v-if="this.addBackgroundType != -1">
                 <h5>Enlace del banner</h5>
                 <input class="form-control" type="text" v-model="this.addSectionValues.t1LinkTo">
               </div>
-              <div v-if="this.addSectionValues.frontType == 2" style="margin-top: 2em">
-                <h3 class="card-subtitle">Configuración de la fila</h3>
+            </div>
+            <div v-if="this.addSectionValues.frontType == 2" style="margin-top: 2em">
+              <h3 class="card-subtitle">Configuración de la fila</h3>
+              <div style="margin-top: 0.5em">
+                <h5>Título*</h5>
+                <input class="form-control" type="text" v-model="this.addSectionValues.t2RowTitle">
+                <h5 style="margin-top: 1em">Número de filas*</h5>
+                <div>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" value=3 v-model="this.addSectionValues.t2RowNumber"
+                           @input="purgeSelectedBooks()">
+                    <label class="form-check-label">3</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" value=4 v-model="this.addSectionValues.t2RowNumber"
+                           @input="purgeSelectedBooks()">
+                    <label class="form-check-label">4</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" value=5 v-model="this.addSectionValues.t2RowNumber"
+                           @input="purgeSelectedBooks()">
+                    <label class="form-check-label">5</label>
+                  </div>
+                </div>
+                <div v-if="this.addSectionValues.t2RowNumber">
+                  <h5 style="margin-top: 1em">Libros a mostrar*</h5>
+                  <select class="form-control" v-model="this.addSectionValues.t2BookMode">
+                    <option value=1>Más vendidos</option>
+                    <option value=2>Precio ascendente</option>
+                    <option value=3>Precio descendente</option>
+                    <option value=0>Selección manual</option>
+                  </select>
+                  <div v-if="this.addSectionValues.t2BookMode == 0">
+                    <div class="row" style="margin-bottom: 1em">
+                      <div class="col">
+                        <h5 style="margin-top: 1.2em">Selecciona {{ addSectionValues.t2RowNumber }} libros* <span
+                            v-if="addSectionValues.t2RowNumber - this.countSelBooks > 0">({{
+                            parseInt(addSectionValues.t2RowNumber) - this.countSelBooks
+                          }} restante<span
+                              v-if="parseInt(addSectionValues.t2RowNumber) - this.countSelBooks != 1">s</span>)</span>
+                        </h5>
+                      </div>
+                      <div class="col">
+                        <select class="form-control pull-right"
+                                @change="this.getBooksFromDBSelector($event.target.value)"
+                                style="margin-top: 1em; width: auto">
+                          <option value="TODO">Todo</option>
+                          <option value="HUMANIDADES">Humanidades</option>
+                          <option value="TECNICO Y FORMACION">Técnico y formación</option>
+                          <option value="METODOS DE IDIOMAS">Métodos de idiomas</option>
+                          <option value="LITERATURA">Literatura</option>
+                          <option value="INFANTIL">Infantil</option>
+                          <option value="COMICS Y MANGA">Cómics y manga</option>
+                          <option value="JUVENIL">Juvenil</option>
+                          <option value="OTRAS CATEGORIAS">Otras categorías</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div class="card" style="height: 30em; overflow-y: auto">
+                      <div class="row row-cols-1 row-cols-md-5" style="margin-left: 0.1em; margin-right: 0.1em">
+                        <div :class="this.getSelBookClassName(item.id)" style="width: 20em; margin: 0.5em"
+                             v-for="item in this.booksSelector"
+                             :key="item.id" :id="item.id" @click="selectBook(item.id)">
+                          <div class="card-body">
+                            <h5 class="card-title">{{ item.name }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ item.author[0] }}</h6>
+                            <span class="badge badge-secondary" v-if="item.genre=='LITERATURA'">Literatura</span>
+                            <span class="badge badge-secondary" v-if="item.genre=='TECNICO Y FORMACION'">Técnico y formación</span>
+                            <span class="badge badge-secondary" v-if="item.genre=='METODOS DE IDIOMAS'">Métodos de idiomas</span>
+                            <span class="badge badge-secondary" v-if="item.genre=='HUMANIDADES'">Humanidades</span>
+                            <span class="badge badge-secondary" v-if="item.genre=='INFANTIL'">Infantil</span>
+                            <span class="badge badge-secondary"
+                                  v-if="item.genre=='COMICS Y MANGA'">Cómics y manga</span>
+                            <span class="badge badge-secondary" v-if="item.genre=='JUVENIL'">Juvenil</span>
+                            <span class="badge badge-secondary"
+                                  v-if="item.genre=='OTRAS CATEGORIAS'">Otras categorías</span>
+                            &nbsp;
+                            <span class="badge badge-info">Stock: {{ item.total_available }}</span>&nbsp;
+                            <span class="badge badge-success">Ventas: {{ item.num_sales }}</span>&nbsp;
+                            <span class="badge badge-dark">{{ item.price }}€</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -172,24 +266,27 @@ export default {
     type: Number
   },
   created() {
-    this.getBooksFromDBL('LITERATURA')
+    scrollTo(0, 0)
+    this.getBooksFromDBSelector('TODO')
   },
   data() {
     return {
-      addBackgroundType: 1,
+      addBackgroundType: -1,
       edit: false,
       loggedIn: false,
       idEditingSection: -1,
       addSectionValues: {
         frontType: -1,
+        t2BookMode: -1,
         t1BackgndURL: '',
-        t1BackgnCOL: '#2bc4ed',
+        t1BackgnCOL: '',
         t1LinkTo: '',
         t1Tit: '',
+        t1Separator: false,
         t1Sub: '',
         t1Smalll: '',
         t2RowTitle: '',
-        t2RowNumber: '',
+        t2RowNumber: -1,
         books: []
       },
       frontElements: [
@@ -200,10 +297,12 @@ export default {
           t1BackgnCOL: '#2bc4ed',
           t1LinkTo: 'https://stackoverflow.com/questions/35242272/vue-js-data-bind-style-backgroundimage-not-working',
           t1Tit: 'Rebajas!',
+          t1Separator: true,
           t1Sub: 'Esto es un sub',
           t1Smalll: 'Letra peq',
           t2RowTitle: '',
           t2RowNumber: '-1',
+          t2BookMode: -1,
           books: []
         },
         {
@@ -213,10 +312,12 @@ export default {
           t1BackgnCOL: '#2bc4ed',
           t1LinkTo: '',
           t1Tit: 'Rebajas!',
+          t1Separator: false,
           t1Sub: 'Esto es un sub',
           t1Smalll: 'Letra peq',
           t2RowTitle: '',
           t2RowNumber: '-1',
+          t2BookMode: -1,
           books: []
         },
         {
@@ -226,10 +327,12 @@ export default {
           t1BackgnCOL: '',
           t1LinkTo: '',
           t1Tit: '',
+          t1Separator: false,
           t1Sub: '',
           t1Smalll: '',
           t2RowTitle: 'Prueba',
           t2RowNumber: '6',
+          t2BookMode: 0,
           books: [{
             "id": 1,
             "ISBN": 9788431690656,
@@ -256,17 +359,83 @@ export default {
       books: [],
       booksR: [],
       booksRR: [],
-      booksRM: []
+      booksRM: [],
+      booksSelector: [],
+      countSelBooks: 0,
+      selectedBooks: []
     }
   }, methods: {
+    purgeSelectedBooks() {
+      Array.from(document.getElementsByClassName('card selected-book')).forEach((el) => el.classList.remove('selected-book'))
+      this.countSelBooks = 0
+      this.selectedBooks = []
+    },
+    canSectionBeSaved() {
+      if (this.addSectionValues.frontType == 1) {
+        if (this.addBackgroundType == 1) {
+          if (this.addSectionValues.t1BackgndURL == '') {
+            return !false
+          } else {
+            return !true
+          }
+        } else if (this.addBackgroundType == 2) {
+          if (this.addSectionValues.t1BackgnCOL == '') {
+            return !false
+          } else {
+            return !true
+          }
+        }
+      } else if (this.addSectionValues.frontType == 2) {
+        if (parseInt(this.addSectionValues.t2RowNumber) >= 3 && parseInt(this.addSectionValues.t2RowNumber) <= 5 && this.addSectionValues.t2RowTitle != '') {
+          if (this.addSectionValues.t2BookMode == 0 && this.selectedBooks.length == parseInt(this.addSectionValues.t2RowNumber)) {
+            return !true
+          } else if (this.addSectionValues.t2BookMode >= 1 && this.addSectionValues.t2BookMode <= 3) {
+            return !true
+          }
+          return !false
+        }
+      }
+      return !false
+    },
+    moveUp(id){
+      console.log(id)
+    },
+    moveDown(id){
+      console.log(id)
+    },
+    selectBook(id) {
+      let div = document.getElementById(id)
+      if (div.classList.contains("selected-book")) {
+        div.classList.remove("selected-book")
+        this.countSelBooks -= 1
+        this.selectedBooks.splice(this.selectedBooks.indexOf(id), 1)
+      } else {
+        if (parseInt(this.addSectionValues.t2RowNumber) > this.countSelBooks) {
+          this.countSelBooks += 1
+          this.selectedBooks.push(id)
+          div.classList.add("selected-book")
+        }
+      }
+      console.log(this.selectedBooks)
+    }
+    ,
+    getSelBookClassName(id) {
+      if (this.selectedBooks.includes(id))
+        return "card selected-book"
+      else
+        return "card"
+    }
+    ,
     discardChanges() {
       this.edit = false
       this.idEditingSection = -1
-    },
+    }
+    ,
     editSection(id) {
       this.idEditingSection = id
       this.edit = true
-    },
+    }
+    ,
     addSection() {
       this.edit = true
       this.addSectionValues = {
@@ -282,55 +451,48 @@ export default {
         books: []
       }
       this.addBackgroundType = -1
-    },
+    }
+    ,
     saveSection() {
       this.edit = false
       this.idEditingSection = -1
-    },
+    }
+    ,
     deleteSection(id) {
       console.log(id)
-    },
+    }
+    ,
     getRowClassName(rows) {
       return "row row-cols-1 row-cols-sm-" + String(rows)
-    },
+    }
+    ,
     compare(a, b) {
       if (a.num_sales < b.num_sales)
         return 1;
       if (a.num_sales > b.num_sales)
         return -1;
       return 0;
-    },
+    }
+    ,
     getYear() {
       return new Date().getFullYear()
-    },
-    getBooksFromDBL(req) {
+    }
+    ,
+    // eslint-disable-next-line no-unused-vars
+    getBooksFromDBSelector(req) {
       var path = api + 'books/' + req
       if (req === 'TODO') {
         path = api + 'books'
       }
       axios.get(path)
           .then((res) => {
-            this.books = res.data.books
-            this.recommendBooks()
-            this.getBooksFromDBR('TODO')
+            this.booksSelector = res.data.books
           })
           .catch((error) => {
             console.log(error) //
           })
-    }, getBooksFromDBR(req) {
-      var path = api + 'books/' + req
-      if (req === 'TODO') {
-        path = api + 'books'
-      }
-      axios.get(path)
-          .then((res) => {
-            this.booksR = res.data.books
-            this.booksRR = this.booksR.sort(this.compare).slice(0, 5)
-          })
-          .catch((error) => {
-            console.log(error) //
-          })
-    },
+    }
+    ,
     recommendBooks() {
       var min = 0, max = this.books.length - 1
       var r1 = 0, r2 = 0, r3 = 0, r4 = 0, r5 = 0
@@ -357,6 +519,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.selected-book {
+  border-color: black;
+  border-width: 0.2em;
+}
+
 .article-column1 {
   -moz-box-shadow: -5px 5px 29px #777777;
   -webkit-box-shadow: -5px 5px 29px #777777;
