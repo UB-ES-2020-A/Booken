e71a9227be44899798144408483b38aa6b3362f5
Abdelkarim Azzouguagh <abdel.azzouguagh@gmail.com>
2020-12-13 11:39:00 +0100

    SEP-logs

--- backend/flask-api/tests/Log_test.py
+++ backend/flask-api/tests/Log_test.py
@@ -1,51 +1,63 @@
 # file deepcode ignore C0413: shut it deepcode
+import base64
+import json
 import os
 import sys
 import unittest
 from datetime import datetime
 
-from models.log import LogModel
-
 parent_path = os.path.dirname(os.path.abspath(__file__))[:-6]
 sys.path.insert(1, parent_path)
 from app import setupApp
 from db import db
+from models.log import LogModel
+from models.accounts import AccountModel
 
 
 class LogModelTests(unittest.TestCase):
+    account_admin_info = {
+        "name": 'Admin',
+        "lastname": 'Admin',
+        "email": "a@a.com",
+        "password": 'sm22'
+    }
 
     def setUp(self):
         self.app = setupApp(True).test_client()
         db.drop_all()
         db.create_all()
+        self.register(self.account_admin_info)
+        self.acc = AccountModel.find_by_email("a@a.com")
+        self.acc.type = 2
+        self.acc.save_to_db()
+        self.resp_account_admin = self.login('a@a.com', 'sm22')
+        self.authorization = {'Authorization': 'Basic ' + base64.b64encode(
+            bytes(
+                str(self.acc.id) + ":" + json.loads(self.resp_account_admin.data)['token'],
+                'ascii')).decode(
+            'ascii')}
 
     def test_model_json(self):
-        self.register('Cristobal', 'Colon', 'tengo@barcos.tech', 'america16')
-        response = self.login('tengo@barcos.tech', 'america16')
         log = LogModel.find_by_id(1)
         self.assertEqual(1, log.json()['id'])
 
-    def test_model_delete_to_db(self):
-        self.register('Cristobal', 'Colon', 'tengo@barcos.tech', 'america16')
-        response = self.login('tengo@barcos.tech', 'america16')
-        asd = LogModel.find_by_id(1).delete_from_db()
-        self.assertEqual(None, LogModel.find_by_id(1))
-
     def test_model_find_by_year(self):
-        self.register('Cristobal', 'Colon', 'tengo@barcos.tech', 'america16')
-        response = self.login('tengo@barcos.tech', 'america16')
-        self.assertEqual(1, LogModel.find_by_year(datetime.now().year)[0].user_id)
+        log = LogModel.find_by_month_year(datetime.now().year, datetime.now().month)[0].user_id
+        eq = LogModel.find_by_year(datetime.now().year)[0].user_id
+        self.assertEqual(1, eq)
 
     def test_get_logs(self):
-        self.register('Cristobal', 'Colon', 'tengo@barcos.tech', 'america16')
-        response = self.login('tengo@barcos.tech', 'america16')
-        response = self.app.get('api/logs', follow_redirects=True)
+        response = self.app.get('api/logs',
+                                headers=self.authorization,
+                                follow_redirects=True)
         self.assertEqual(200, response.status_code)
 
-    def register(self, name, lname, email, password):
-        return self.app.post('api/account',
-                             data=dict(name=name, lastname=lname, email=email, password=password),
-                             follow_redirects=True)
+    def test_model_delete_to_db(self):
+        log = LogModel.find_by_id(1).delete_from_db()
+        self.assertEqual(None, LogModel.find_by_id(1))
+
+    def register(self, info):
+        return self.app.post('api/account', data=info, follow_redirects=True)
 
     def login(self, email, password):
         return self.app.post('api/login',
